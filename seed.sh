192813aab9679d4c708f35c9b6f2bcb97145253b
buzzkillb <travanx@gmail.com>
2019-09-24 11:26:32 -0700

    path

--- seed.sh
+++ seed.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/cf-seeder
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/duct-tape-dns-seeder
 grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' "${1:?}" |
 while read IP
 do
