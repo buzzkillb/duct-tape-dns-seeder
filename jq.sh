a1b1b5455bd8fa86f9e8171f66c5441403399496
buzzkillb <travanx@gmail.com>
2019-09-24 11:36:48 -0700

    remove brackets and port ipv6

--- jq.sh
+++ jq.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jq '.[] | .addr' -r /root/duct-tape-dns-seeder/peer.json > /root/duct-tape-dns-seeder/peers.txt
+jq '.[] | .addr' -r /root/duct-tape-dns-seeder/peer.json | sed 's/.*\[\([^]]*\)\].*/\1/g' > /root/duct-tape-dns-seeder/peers.txt
